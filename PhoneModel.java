b2d8ed0171cffb0f5c9f97298574890ccaccd44d
Matheus Leite <matheusleitecordeiro@gmail.com>
2026-09-24 01:29:50 -0300

    feat: passo 2 do plano - altera PhoneModel para notificar observadores

--- PhoneModel.java
+++ PhoneModel.java
@@ -5,13 +5,25 @@ import java.util.List;
  * Store a phone number, digit-by-digit
  */
 public class PhoneModel {
-    private List<Integer> digits = new ArrayList<>();
+    private final List<Integer> digits = new ArrayList<>();
+    private final List<PhoneObserver> observers = new ArrayList<>();
+
+    public void addObserver(PhoneObserver observer) {
+        observers.add(observer);
+    }
 
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        notifyObservers(newDigit);
     }
 
     public List<Integer> getDigits() {
         return digits;
+    }
+
+    private void notifyObservers(int newDigit) {
+        for (PhoneObserver observer : observers) {
+            observer.onDigit(newDigit);
+        }
     }
 }
